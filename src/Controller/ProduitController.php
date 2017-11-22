135bb2b61bbf64ab43bc2ad463a71c187a6174f6
Starburste <vincentramousset@gmail.com>
2017-11-22 15:18:19 +0100

    Terminaison du cote backOff + recherche via liste deroulante --> terminaison cote frontOff

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -165,6 +165,7 @@ class ProduitController implements ControllerProviderInterface
                 'typeProduit_id' =>htmlspecialchars($_POST['typeProduit_id'])
             ];
         }else{
+            //var_dump($typeProduit);
             return $app['twig']->render('frontOff/client/researchProduit.html.twig',['typeProduits'=> $typeProduit]);
         }
 
